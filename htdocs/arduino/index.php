8b17356f4413da42600dbd73afaa182e730ea115
HyeRin <08291111@naver.com>
2017-05-17 15:16:29 +0900

    Adding file download functionality to php

--- htdocs/arduino/index.php
+++ htdocs/arduino/index.php
@@ -6,7 +6,12 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-
+	<script>
+	function downloadTXT(){
+		document.location.href='/arduino/data.txt';
+		return false;
+		}
+	</script>
 	<style>
 	.tbl_type,.tbl_type th,.tbl_type td{border:0}
 	.tbl_type{width:100%;border-bottom:2px solid #dcdcdc;font-family:Tahoma;font-size:11px;text-align:center}
@@ -16,13 +21,14 @@
 	</style>
 		</head>
 	<body>
+				<form method=GET name=frm1 action='index.php'>
 		<h1>Aruduino Data
-		<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+		<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" value = "save" onclick="return downloadTXT()">
 		<i class="material-icons">save</i>
 		</button>
 		</h1>
 		<div style="padding:10px;"> 
-			<form method=GET name=frm1 action='index.php'>
+
 			센서 이름 : 
 				<div class="mdl-textfield mdl-js-textfield">
 				<input class="mdl-textfield__input" type="text" name="sensor_id" >
@@ -93,22 +99,38 @@
 		$sql = "SELECT * FROM `arduino_data` WHERE sensor_id = '$sensor_id' ORDER BY id DESC LIMIT 100";
 		$result = mysql_query($sql);
 		if ( !mysql_num_rows($result) ) exit('데이터가 없습니다'); 
-
+			
+		$myfile = fopen("data.txt", "w") or die("Unable to open file!");
+		
+		$txt = "";
+		
 		while($line = mysql_fetch_array($result))
 		{
 			echo ' <tr>';
 			echo '<td>'.$line["id"].'</td>';
+			$txt .= "{$line["id"]}" . " ";
 			echo '<td>'.$line["sensor_id"].'</td>';
+			$txt .= "{$line["sensor_id"]}" . " ";
 			echo '<td>'.$line["time"].'</td>';
+			$txt .= "{$line["time"]}" . " ";
 			echo '<td>'.$line["AcX"].'</td>';
+			$txt .= "{$line["AcX"]}" . " ";
 			echo '<td>'.$line["AcY"].'</td>';
+			$txt .= "{$line["AcY"]}" . " ";
 			echo '<td>'.$line["AcZ"].'</td>';
+			$txt .= "{$line["AcZ"]}" . " ";
 			echo '<td>'.$line["Tmp"].'</td>';
+			$txt .= "{$line["Tmp"]}" . " ";
 			echo '<td>'.$line["GyX"].'</td>';
+			$txt .= "{$line["GyX"]}" . " ";
 			echo '<td>'.$line["GyY"].'</td>';
+			$txt .= "{$line["GyY"]}" . " ";
 			echo '<td>'.$line["GyZ"].'</td>';
+			$txt .= "{$line["GyZ"]}" . "\n";
 			echo'</tr>';
 		}
+		fwrite($myfile, $txt);
+		fclose($myfile);
 	}
 	else if(empty($sensor_id) && !empty($time_from))//데이터만 입력되었을때
 	{		
@@ -121,22 +143,38 @@
 		$sql = "SELECT * FROM `arduino_data` WHERE time BETWEEN '$new_time_from' AND '$new_time_until' ORDER BY id DESC LIMIT 100";
 		$result = mysql_query($sql);
 		if ( !mysql_num_rows($result) ) exit('데이터가 없습니다'); 
+		
+		$myfile = fopen("data.txt", "w") or die("Unable to open file!");
+		
+		$txt = "";
 
 		while($line = mysql_fetch_array($result))
 		{
 			echo ' <tr>';
 			echo '<td>'.$line["id"].'</td>';
+			$txt .= "{$line["id"]}" . " ";
 			echo '<td>'.$line["sensor_id"].'</td>';
+			$txt .= "{$line["sensor_id"]}" . " ";
 			echo '<td>'.$line["time"].'</td>';
+			$txt .= "{$line["time"]}" . " ";
 			echo '<td>'.$line["AcX"].'</td>';
+			$txt .= "{$line["AcX"]}" . " ";
 			echo '<td>'.$line["AcY"].'</td>';
+			$txt .= "{$line["AcY"]}" . " ";
 			echo '<td>'.$line["AcZ"].'</td>';
+			$txt .= "{$line["AcZ"]}" . " ";
 			echo '<td>'.$line["Tmp"].'</td>';
+			$txt .= "{$line["Tmp"]}" . " ";
 			echo '<td>'.$line["GyX"].'</td>';
+			$txt .= "{$line["GyX"]}" . " ";
 			echo '<td>'.$line["GyY"].'</td>';
+			$txt .= "{$line["GyY"]}" . " ";
 			echo '<td>'.$line["GyZ"].'</td>';
+			$txt .= "{$line["GyZ"]}" . "\n";
 			echo'</tr>';
 		}
+		fwrite($myfile, $txt);
+		fclose($myfile);
 	}
 	else if(!empty($sensor_id) && !empty($time_from))//둘다 입력되었을때
 	{
@@ -150,46 +188,78 @@
 		
 		$sql = "SELECT * FROM `arduino_data` WHERE sensor_id = '$sensor_id' AND (time BETWEEN '$new_time_from' AND '$new_time_until') ORDER BY id DESC LIMIT 100";
 		$result = mysql_query($sql);
-		if ( !mysql_num_rows($result) ) exit('데이터가 없습니다'); 
+		if ( !mysql_num_rows($result) ) exit('\n데이터가 없습니다'); 
+				
+		$myfile = fopen("data.txt", "w") or die("Unable to open file!");
+		
+		$txt = "";
 
 		while($line = mysql_fetch_array($result))
 		{
 			echo ' <tr>';
 			echo '<td>'.$line["id"].'</td>';
+			$txt .= "{$line["id"]}" . " ";
 			echo '<td>'.$line["sensor_id"].'</td>';
+			$txt .= "{$line["sensor_id"]}" . " ";
 			echo '<td>'.$line["time"].'</td>';
+			$txt .= "{$line["time"]}" . " ";
 			echo '<td>'.$line["AcX"].'</td>';
+			$txt .= "{$line["AcX"]}" . " ";
 			echo '<td>'.$line["AcY"].'</td>';
+			$txt .= "{$line["AcY"]}" . " ";
 			echo '<td>'.$line["AcZ"].'</td>';
+			$txt .= "{$line["AcZ"]}" . " ";
 			echo '<td>'.$line["Tmp"].'</td>';
+			$txt .= "{$line["Tmp"]}" . " ";
 			echo '<td>'.$line["GyX"].'</td>';
+			$txt .= "{$line["GyX"]}" . " ";
 			echo '<td>'.$line["GyY"].'</td>';
+			$txt .= "{$line["GyY"]}" . " ";
 			echo '<td>'.$line["GyZ"].'</td>';
+			$txt .= "{$line["GyZ"]}" . "\n";
 			echo'</tr>';
 		}
+		fwrite($myfile, $txt);
+		fclose($myfile);
 	}
 	else
 	{
 
 		$result = mysql_query("SELECT * FROM `arduino_data` ORDER BY id DESC LIMIT 100");
+		
+		$myfile = fopen("data.txt", "w") or die("Unable to open file!");
+		
+		$txt = "";
 
 		while($line = mysql_fetch_array($result))
 		{
 			echo ' <tr>';
-				echo '<td>'.$line["id"].'</td>';
-				echo '<td>'.$line["sensor_id"].'</td>';
-				echo '<td>'.$line["time"].'</td>';
-				echo '<td>'.$line["AcX"].'</td>';
-				echo '<td>'.$line["AcY"].'</td>';
-				echo '<td>'.$line["AcZ"].'</td>';
-				echo '<td>'.$line["Tmp"].'</td>';
-				echo '<td>'.$line["GyX"].'</td>';
-				echo '<td>'.$line["GyY"].'</td>';
-				echo '<td>'.$line["GyZ"].'</td>';
+			echo '<td>'.$line["id"].'</td>';
+			$txt .= "{$line["id"]}" . " ";
+			echo '<td>'.$line["sensor_id"].'</td>';
+			$txt .= "{$line["sensor_id"]}" . " ";
+			echo '<td>'.$line["time"].'</td>';
+			$txt .= "{$line["time"]}" . " ";
+			echo '<td>'.$line["AcX"].'</td>';
+			$txt .= "{$line["AcX"]}" . " ";
+			echo '<td>'.$line["AcY"].'</td>';
+			$txt .= "{$line["AcY"]}" . " ";
+			echo '<td>'.$line["AcZ"].'</td>';
+			$txt .= "{$line["AcZ"]}" . " ";
+			echo '<td>'.$line["Tmp"].'</td>';
+			$txt .= "{$line["Tmp"]}" . " ";
+			echo '<td>'.$line["GyX"].'</td>';
+			$txt .= "{$line["GyX"]}" . " ";
+			echo '<td>'.$line["GyY"].'</td>';
+			$txt .= "{$line["GyY"]}" . " ";
+			echo '<td>'.$line["GyZ"].'</td>';
+			$txt .= "{$line["GyZ"]}" . "\n";
 			echo'</tr>';
 		}
+		fwrite($myfile, $txt);
+		fclose($myfile);
 	}
-	?>
+?>
 			</tr>
 			</tbody>
 			</table>
